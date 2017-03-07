e52b3c9824a7d7f15bd6e0c08ad40278da71df24
nagylilla96 <n.lilla@yahoo.com>
2017-03-07 12:48:20 +0200

    initial commit

---
+++ DemoProject/src/main/java/PT2017/demo/DemoProject/App.java
@@ -0,0 +1,13 @@
+package PT2017.demo.DemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
